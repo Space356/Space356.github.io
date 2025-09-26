f23c2acb2660949616bb9fa6811bc58f9a87b665
Space356 <155692669+Space356@users.noreply.github.com>
2025-09-26 02:13:50 +0000

    dicwiz order fixing

--- dictionary_wizard/index.js
+++ dictionary_wizard/index.js
@@ -1,32 +1,31 @@
 var result = "";
 var curr_num = 0;
-function generate()
+async function generate()
 {
     result = "";
     curr_num = 0;
     result += "<ol>";
     const words = document.getElementById("inpt").value.split(",");
-    words.forEach(element => {
+    for (const element of words)
+    {
         const fetch_string = "https://api.dictionaryapi.dev/api/v2/entries/en/"+element;
-        fetch(fetch_string,
+        const response = await fetch(fetch_string,{mode: "cors"})
+        const data = await response.json();
+
+        console.log(data[0]);
+        curr_num ++;
+        result += "<li>"+data[0].word+": <ul>";
+        for (let i=0;i<data[0].meanings.length;i++)
+        {
+            for (let j=0;j<data[0].meanings[i].definitions.length;j++)
             {
-              mode: "cors"
-            })
-            .then(response => response.json())
-            .then(data => {
-                console.log(data[0]);
-                curr_num ++;
-                result += "<li>"+data[0].word+": <ul>";
-                for (let i=0;i<data[0].meanings.length;i++)
-                {
-                    for (let j=0;j<data[0].meanings[i].definitions.length;j++)
-                    {
-                        result += "<li>"+data[0].meanings[i].partOfSpeech+" - "+data[0].meanings[i].definitions[j].definition+"</li>     ";
-                    }
-                }
-                result += "</ul></li>";
-            });
-    });
+                result += "<li>"+data[0].meanings[i].partOfSpeech+" - "+data[0].meanings[i].definitions[j].definition+"</li>     ";
+            }
+        }
+        result += "</ul></li>";
+
+        //await Promise.all(promises);
+    }
 }
 
 //document.getElementById("result").innerHTML = result;
